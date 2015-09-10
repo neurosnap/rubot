b24220220363984a2659fa2bc5310e622071b0de
Eric Bower <neurosnap@gmail.com>
2015-09-10 16:28:03 -0400

    chye

--- README.md
+++ README.md
@@ -13,9 +13,25 @@ Message or add [@rubot_bot](http://telegram.org) on telegram
 ```
 
 ```
+/tz list 3
+```
+
+```
+/tz search asia
+/tz search america
+```
+
+```
 /tz America/Detroit
 ```
 
 ```
 /tz Asia/Novosibirsk
+```
+
+```
+/andrey
+/mi
+/joshua
+/mal
 ```
--- rubot.js
+++ rubot.js
@@ -15,43 +15,82 @@ bot.getMe().then(function(obj) {
 bot.on('text', function(msg) {
   var chatId = msg.chat.id;
   var messageId = msg.message_id;
-
   var format = "hh:mm:ss a";
+
   var timezone;
-  var time;
+  var out_msg = "Current time: ";
   if (msg.text == '/andrey') {
-    // Asia/Novosibirsk
     timezone = "Asia/Novosibirsk";
-    time = moment().tz(timezone).format(format);
+    out_msg += moment().tz(timezone).format(format);
+    return bot.sendMessage(chatId, out_msg);
   }
 
   if (msg.text == '/mi') {
-    // America/Detroit
     timezone = "America/Detroit";
-    time = moment().tz(timezone).format(format);
+    out_msg += moment().tz(timezone).format(format);
+    return bot.sendMessage(chatId, out_msg);
+  }
+
+  if (msg.text == '/joshua') {
+    timezone = "CST6CDT";
+    out_msg += moment().tz(timezone).format(format);
+    return bot.sendMessage(chatId, out_msg);
+
+  }
+
+  if (msg.text == '/mal') {
+    timezone = "America/Los_Angeles";
+    out_msg += moment().tz(timezone).format(format);
+    return bot.sendMessage(chatId, out_msg);
+
   }
 
   var tz_i = msg.text.indexOf("/tz");
   if (tz_i >= 0) {
     timezone = msg.text.slice(tz_i + 3).trim();
-    console.log(timezone);
 
-    if (timezone == "list") {
-      return bot.sendMessage(chatId, listToStrings(moment.tz.names()));
+    var list_i = timezone.indexOf("list");
+    if (list_i >= 0) {
+      var list = moment.tz.names();
+      var tz_per_page = 20;
+      var page = parseInt(timezone.slice(list_i + 4).trim());
+      if (isNaN(page)) page = 1;
+      var total_pages = parseInt((list.length / tz_per_page) + 1);
+
+      if (page < 1 || page > total_pages) {
+        return bot.sendMessage(chatId, "Page number must be greater than 0 and less than " + total_pages);
+      }
+
+      out_msg = "";
+      for (var i = (page-1)*tz_per_page; i < page*tz_per_page && i < list.length; i++) {
+        out_msg += list[i] + "\n";
+      }
+      out_msg += "Page " + page + " out of " + total_pages;
+
+      return bot.sendMessage(chatId, out_msg);
     }
 
-    time = moment().tz(timezone).format(format);
-  }
+    var search_i = timezone.indexOf("search");
+    if (search_i >= 0) {
+      var list = moment.tz.names();
+      var search = timezone.slice(search_i + 6).trim().toLowerCase();
+      var min_chars = 3;
+      if (search.length <= min_chars) {
+        return bot.sendMessage(chatId, "Search must be at least " + min_chars + " long");
+      }
 
-  bot.sendMessage(chatId, time);
+      out_msg = "";
+      for (var i = 0; i < list.length; i++) {
+        if (list[i].toLowerCase().indexOf(search) >= 0) {
+          out_msg += list[i] + "\n";
+        }
+      }
+
+      return bot.sendMessage(chatId, out_msg);
+    }
+
+    out_msg += moment().tz(timezone).format(format);
+    return bot.sendMessage(chatId, out_msg);
+  }
 });
 
-function listToString(arr) {
-  var content = "";
-
-  for (var i = 0; i < arr.length; i++) {
-    content += arr[i] + "\n";
-  }
-
-  return content;
-}
